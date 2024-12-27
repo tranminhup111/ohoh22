fc1fb1c9560f49f36612b374e2558499ce603376
tranminhup111 <139839938+tranminhup111@users.noreply.github.com>
2024-12-27 11:38:17 +0700

    Update Demo.java
    fff111

--- 19JSP02/src/jsp/examples/bt2/Demo.java
+++ 19JSP02/src/jsp/examples/bt2/Demo.java
@@ -2,7 +2,7 @@ package jsp.examples.bt2;
 
 public class Demo {
 
-	public static void main(String[] args) {
+	public static void main1111(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
